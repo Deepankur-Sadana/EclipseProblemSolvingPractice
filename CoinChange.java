1d133cf60398d17b2077f76e678600b4da587485
deepankursadana <writetodeepankursadana@gmail.com>
2018-07-10 23:15:05 +0530

    left view, bottomVieww and 1,2,3 steps problem done

--- CoinChange.java
+++ CoinChange.java
@@ -7,7 +7,7 @@ public class CoinChange {
 
 	static void changeCoin() {
 
-		changeCoin(new int[] { 1, 2, 3 }, 3);
+		climbStairs(new int[] { 1, 2, 3 }, 6);
 	}
 
 	static void changeCoin(int[] coins, int sum) {
@@ -22,14 +22,35 @@ public class CoinChange {
 		for (int i = 1; i < coins.length; i++) {
 			for (int curSum = 0; curSum <= sum; curSum++) {
 				//
-				dp[i][curSum] = curSum < coins[i] ?
-						dp[i - 1][curSum] :
-							dp[i - 1][curSum] + dp[i][curSum - coins[i]];					
+				dp[i][curSum] = curSum < coins[i] ? dp[i - 1][curSum] : dp[i - 1][curSum] + dp[i][curSum - coins[i]];
 			}
 
 		}
 		Utils.pint(dp);
 
+	}
+
+	static void climbStairs(int[] steps, int height) {
+		int dp[][] = new int[steps.length][height + 1];
+
+		for (int i = 0; i < dp[0].length; i++) {
+			dp[0][i] = 1;
+		}
+
+		for (int i = 0; i < dp.length; i++) {
+			dp[i][0] = 1;
+		}
+
+		for (int i = 1; i < steps.length; i++) {
+			for (int curStep = 1; curStep <= height; curStep++) {
+				if (curStep < 2)
+					dp[i][curStep] = 1;
+				else
+					dp[i][curStep] = dp[i][curStep - 1] +  dp[i][curStep - 2];
+			}
+
+		}
+		Utils.pint(dp);
 	}
 
 }
--- TeeeGrraph2.java
+++ TeeeGrraph2.java
@@ -2,15 +2,6 @@ import java.util.*;
 
 public class TeeeGrraph2 {
 
-	public static void main(String[] args) {
-		TeeeGrraph2 t = new TeeeGrraph2();
-
-		Node root =t.makeTree();
-		t.printLeftView(root, 1, new HashMap<Integer, Boolean>());
-	}
-
-
-
 	public static class Node {
 		Node left, right;
 		int data;
@@ -37,7 +28,7 @@ public class TeeeGrraph2 {
 
 	}
 
-	 void printLeftView(Node root, int level, HashMap<Integer, Boolean> map) {
+	void printLeftView(Node root, int level, HashMap<Integer, Boolean> map) {
 		if (root == null) {
 			return;
 		}
@@ -51,4 +42,75 @@ public class TeeeGrraph2 {
 
 	}
 
+	Node buildBottomViewTree() {
+		Node n20 = new Node(20);
+		n20.right = new Node(22);
+		n20.right.right = new Node(25);
+		n20.right.left = new Node(4);
+
+		n20.left = new Node(8);
+		n20.left.left = new Node(5);
+		
+
+		n20.left.right = new Node(3);
+		n20.left.right.left = new Node(10);
+		n20.left.right.right= new Node(14);
+
+		return n20;
+	}
+
+	/**
+	 * 
+	 *                20
+                    /    \
+                  8       22
+                /   \      \
+              5      3      25
+                    / \      
+                  10    14
+
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		TeeeGrraph2 t = new TeeeGrraph2();
+
+		Node root = t.makeTree();
+		t.printBottomView();
+	}
+
+	void printBottomView() {
+		Node root = buildBottomViewTree();
+		traverse(root, 1, 0);
+		printBottomOrder();
+	}
+
+	TreeMap<Integer, TreeMap<Integer, Integer>> map = new TreeMap<>(); // <horizontal.<level,data>> map
+
+	void traverse(Node root, int level, int horizontalDistance) {
+		if (root == null)
+			return;
+		traverse(root.left, level + 1, horizontalDistance - 1);
+		System.out.println("" + root.data);
+		addNodeToMap(root,level,horizontalDistance);
+		traverse(root.right, level + 1, horizontalDistance + 1);
+	}
+
+	void addNodeToMap(Node root, int level, int hor) {
+		if (map.get(hor) == null)
+			map.put(hor, new TreeMap<Integer, Integer>(Collections.reverseOrder()));
+
+		map.get(hor).put(level, root.data);
+	}
+
+	void printBottomOrder() {
+		for (Map.Entry<Integer, TreeMap<Integer, Integer>> column : map.entrySet()) {
+			for(Map.Entry<Integer, Integer> element:column.getValue().entrySet()) {
+				System.out.println("---"+element.getValue());
+				break;
+			}
+
+		}
+
+	}
 }
