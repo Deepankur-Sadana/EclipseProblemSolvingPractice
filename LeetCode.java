7618a429ed52e1ac935b5419891f3f7776059be1
Deepankur <writetodeepankursadana@gmail.com>
2018-09-28 07:18:40 +0530

    merge k lists wrong

--- LeetCode.java
+++ LeetCode.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class LeetCode {
 
-
 	public int maxCoins(int[] nums) {
 		ArrayList<Integer> list = new ArrayList<Integer>();
+
 		for (int i : nums) {
 			list.add(i);
 		}
@@ -71,7 +71,7 @@ public class LeetCode {
 	}
 
 	void swim(Point p, int[][] grid, int[][] dis, boolean[][] marked, Queue<Point> queue) {
-		
+
 		marked[p.r][p.c] = true;
 //                System.out.println(" r "+p.r+" c "+p.c);
 
@@ -80,8 +80,8 @@ public class LeetCode {
 		for (Point n : neighbours) {
 			if (!isValidPoint(n, grid))
 				continue;
-			if (!isOptimal(grid,dis,n,p))
-			continue;
+			if (!isOptimal(grid, dis, n, p))
+				continue;
 			queue.add(n);
 			dis[n.r][n.c] = Math.min(dis[n.r][n.c], Math.max(grid[n.r][n.c], dis[p.r][p.c]));
 		}
@@ -93,11 +93,11 @@ public class LeetCode {
 
 	boolean isOptimal(int[][] grid, int[][] dis, Point end, Point start) {
 		Point p = start, n = end;
-		if(dis[end.r][end.c] > dis[start.r][start.c] && grid[end.r][end.c]<  dis[start.r][start.c])
-		return true;
-		
+		if (dis[end.r][end.c] > dis[start.r][start.c] && grid[end.r][end.c] < dis[start.r][start.c])
+			return true;
+
 		return false;
-		
+
 	}
 
 	Point[] getNeighbours(Point p) {
@@ -119,50 +119,144 @@ public class LeetCode {
 			this.r = r;
 			this.c = c;
 		}
-		
-	}
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		LeetCode l = new LeetCode();
-//		int c = l.maxCoins(new int[] {3,1,5,8});
-//		System.out.println("max "+c);
-		
-		l.isMatch("adceb","*a*b");
 
 	}
 
-    public boolean isMatch(String s, String p) {
-        boolean[][] arr = new boolean[s.length()+1][p.length()+1];
-        arr[0][0] = true;
-        for(int i=1; i< arr[0].length;i++) {
-        	 
-        	if(p.charAt(i-1) == '*'){
-                arr[0][i]= arr[0][i-1];
-                
-            }
-        }
-        
-        for(int i= 1 ; i < arr.length;i++){
-            for(int j = 1 ; j < arr[0].length ; j ++){   
-                char target = s.charAt(i-1);
-                char pattern = p.charAt(j-1);
-                if(target == pattern || pattern == '?'){
-                    arr[i][j]=arr[i-1][j-1];
-                }else if(pattern == '*'){
-                    arr[i][j]= arr[i-1][j]||arr[i][j-1] ;
-                    
-                }
-            }
-        }
-        Utils.print(arr);
-        return arr[s.length()][p.length()];
+	public boolean isMatch(String s, String p) {
+		boolean[][] arr = new boolean[s.length() + 1][p.length() + 1];
+		arr[0][0] = true;
+		for (int i = 1; i < arr[0].length; i++) {
+
+			if (p.charAt(i - 1) == '*') {
+				arr[0][i] = arr[0][i - 1];
+
+			}
+		}
+
+		for (int i = 1; i < arr.length; i++) {
+			for (int j = 1; j < arr[0].length; j++) {
+				char target = s.charAt(i - 1);
+				char pattern = p.charAt(j - 1);
+				if (target == pattern || pattern == '?') {
+					arr[i][j] = arr[i - 1][j - 1];
+				} else if (pattern == '*') {
+					arr[i][j] = arr[i - 1][j] || arr[i][j - 1];
+
+				}
+			}
+		}
+		Utils.print(arr);
+		return arr[s.length()][p.length()];
+	}
+
+
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+
+
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        ListNode node1_1 = new ListNode(1);
+        ListNode node3 = new ListNode(3);
+        ListNode node4_1 = new ListNode(4);
+
+        ListNode node2 = new ListNode(2);
+        ListNode node6 = new ListNode(6);
+
+        node1.next = node4;
+        node4.next = node5;
+
+        node1_1.next = node3;
+        node3.next = node4_1;
+
+        node2.next = node6;
+
+        System.out.println(node1);
+        System.out.println(node1_1);
+        System.out.println(node2);
+
+//        ListNode node = new MergeKSortedLists().mergeKLists(new ListNode[]{node1, node1_1});
+    	LeetCode l = new LeetCode();
+        l.mergeKLists(new ListNode[] {node1,node1_1,node2});
     }
     
-    
+    ListNode node, head;
+    public ListNode mergeKLists(ListNode[] lists) {
+       int i=0;
+        ListNode poll;
+        while(1==1){
+            ++i;
+           poll = pollSmallest(lists);
+            if(poll == null) return head;;
+            
+            System.out.println(poll.val);
+//    printList(lists);
+            if(head == null) {
+                head = poll;
+                node = poll;   
+            }
+            else node.next = poll;
+            
+            if(node != null)
+                node = node.next;
+        }
+//        return head;
+    }
+    ListNode pollSmallest(ListNode[] lists) {
+        int small = -1;
+        
+        for(int i= 0 ; i < lists.length; i++){
+            if(lists[i] == null) 
+                continue;
+            
+            if(small == -1 ){
+                small = i;
+                continue;
+            }
+            
+            if(lists[small].val > lists[i].val)    
+                small = i;
+        }
+        
+        if(small == -1) return null;
+        
+        ListNode temp = lists[small] ;
+        lists[small] = lists[small].next;
+        temp.next=null;
+        return temp;
+        
+    }
+    void printList(ListNode[] lists){
+        ListNode n;
+         for(int i= 0 ; i < lists.length; i++){
+             if(lists[i]==null)continue;
+              n = lists[i];
+             String  b ="";
+             while(n != null){
+                 b+=n.val;
+                 b+=" ";
+                 n=n.next;
+                 
+             }
+             System.out.println(b);
+         }
+    }
 }
 
-
-
+	
+	
+	
+	
+	
--- Utils.java
+++ Utils.java
@@ -25,4 +25,43 @@ public class Utils {
 		}
 
 	}
+
+	public static void main(String args[]) {
+		quickSort();
+	}
+
+	static void quickSort() {
+		int[] arr = new int[] {2,3,1,9,5,8,4};
+		quickSort(0,arr.length,arr);
+		pint(arr);
+	}
+
+	static void quickSort(int l, int r, int[] arr) {
+		if (r >= l)
+			return;
+
+		int pivot = r;
+		int i = l;
+		int j = r - 1;
+		while (i <= j) {
+			if (arr[j] < arr[j]) {
+				while (arr[j] > arr[pivot]) {
+					j--;
+				}
+				swap(arr, i, j);
+			}
+
+		}
+		swap(arr, j, pivot);
+		
+		quickSort(l, pivot - 1, arr);
+		quickSort(pivot + 1, r, arr);
+
+	}
+
+	static void swap(int arr[], int a, int b) {
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
+	}
 }
